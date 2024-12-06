bfdd0437b0585c7269743d060ed0f82c3ee4bc23
Abimael <mabimael700@gmail.com>
2024-12-06 16:42:51 -0600

    socket messages refactorized

--- src/lib/emit-results-websocket.js
+++ src/lib/emit-results-websocket.js
@@ -4,7 +4,7 @@ const { getIO } = require("../middlewares/servers/websocket-server");
 function emitResultsToWebSocket(parsedContent) {
     const io = getIO();
     if (io) {
-        io.emit("labResultsMessage", { data: JSON.stringify(parsedContent) });
+        io.emit("message", JSON.stringify({ data: parsedContent, channel: "lab_result" }));
     }
 }
 
